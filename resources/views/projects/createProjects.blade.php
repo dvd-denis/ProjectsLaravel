cfb8936cf7f615fafc2043cf07b8a8bd315e8c99
dvd-denis <pikuldenis9@gmail.com>
2022-05-24 17:18:45 +0600

    test

--- resources/views/projects/createProjects.blade.php
+++ resources/views/projects/createProjects.blade.php
@@ -4,11 +4,35 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+            <div class="card bg-dark text-white border">
+                <div class="card-header border-bottom">Create project</div>
 
                 <div class="card-body">
-                    Вы успешно вошли!!!
+                    <form method="POST" action="{{ route('createStudent') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Имя</label>
+
+                            <div class="col-md-6">
+                                <input placeholder="Name" id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Create
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
